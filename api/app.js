9349c831536113183f57199471c1f002a0a624e9
VictorSarov <victor.sarov@rentalmatics.com>
2026-05-02 12:28:15 +0100

    all good

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.json([
     {
       "id":"1",
-      "title":"Book Review: The Bear & The Nightingale...test4"
+      "title":"Book Review: The Bear & The Nightingale...test11"
     },
     {
       "id":"2",
