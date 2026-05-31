54cce96f40fe9c4ea5b391aac30b07ec64edc287
VictorSarov <victor.sarov@rentalmatics.com>
2026-05-31 09:58:13 +0100

    good stuff

---
+++ app/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [blogs, setBlogs] = useState<any>([]);
+    useEffect(() => {
+        fetch("http://localhost:4000/")
+            .then((res) => res.json())
+            .then((data) => setBlogs(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>all blogs CHANGE</h1>
+                <ul>
+                  {!blogs && <p>Loading...</p>}
+                    {blogs &&
+                        blogs.map((blog: any) => (
+                            <li key={blog.id}>{blog.title}</li>
+                        ))}
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+export default App;
